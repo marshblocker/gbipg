88ddb2e4173aa373e7ca857650ef18100de9361e
Marshblocker <glmarinas@up.edu.ph>
2022-05-15 16:47:27 +0800

    Added overlap methods to Point class

--- classes.py
+++ classes.py
@@ -2,6 +2,9 @@ from const import *
 from utils import linear_distance
 
 class Point:
+    ''' 
+    Represents the center point of a circle to be generated on the Ishihara Plate.
+    '''
     def __init__(self, x, y):
         self._x = x
         self._y = y
@@ -19,8 +22,28 @@ class Point:
     def set_coord(self, new_coord):
         self._x, self._y = new_coord
         self.set_loc()
-       
-    def check_collision(self, p2):
-        return linear_distance(self, p2) > MIN_CIRCLE_RADIUS
+
+    def will_overlap_point(self, p2):
+        return self._linear_distance(self, p2) <= MIN_CIRCLE_RADIUS
+    
+    def will_overlap_wall(self):
+        px, py = self.get_coord()
+        
+        if px <= MIN_CIRCLE_RADIUS or WIDTH - px <= MIN_CIRCLE_RADIUS:
+            return True
+        
+        if py <= MIN_CIRCLE_RADIUS or HEIGHT - px <= MIN_CIRCLE_RADIUS:
+            return True
+        
+        return False
+        
+    def _linear_distance(self, p2):
+        '''
+        Calculates the x-value distance and y-value distance between
+        this point and p2, and returns the maximum between the two.
+        '''
+        p1x, p1y = self.get_coord()
+        p2x, p2y = p2.get_coord()
+        return max([abs(p2x - p1x), abs(p2y - p1y)])
         
     
--- utils.py
+++ utils.py
@@ -1,5 +1,1 @@
-def linear_distance(p1, p2):
-    p1x, p1y = p1.get_coord()
-    p2x, p2y = p2.get_coord()
-    
-    return max([abs(p2x - p1x), abs(p2y - p1y)])
+
