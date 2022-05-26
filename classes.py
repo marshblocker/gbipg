5cd0a30defaaebd586c4974cf2360efbfbd34788
Marshblocker <glmarinas@up.edu.ph>
2022-05-26 17:31:20 +0800

    Image will not be automatically drawn to screen

--- classes.py
+++ classes.py
@@ -3,29 +3,31 @@ import random as rand
 from const import *
 import utils
 
+
 class Point:
     ''' 
     Represents the center point of a circle to be generated on the Ishihara Plate.
     '''
+
     def __init__(self, x, y, canvas_pxls):
         self._x = x
         self._y = y
         self._loc = WIDTH*self._y + self._x
         self._in_fig = (canvas_pxls[self.get_loc()] == BLACK_RGB)
-        
+
     def get_loc(self):
         return self._loc
-        
+
     def _set_loc(self):
         self._loc = WIDTH*self._y + self._x
-        
+
     def get_coord(self):
         return (self._x, self._y)
-   
+
     def set_coord(self, new_coord):
         self._x, self._y = new_coord
         self._set_loc()
-        
+
     def in_fig(self):
         return self._in_fig
 
@@ -33,46 +35,48 @@ class Point:
         p_coord = self.get_coord()
         p2_coord = p2.get_coord()
         return utils.distance_squared(p_coord, p2_coord) <= (2 * MIN_CIRCLE_RADIUS)**2
-    
+
     def will_overlap_wall(self):
         px, py = self.get_coord()
-        
+
         if px <= MIN_CIRCLE_RADIUS or WIDTH - px <= MIN_CIRCLE_RADIUS:
             return True
-        
+
         if py <= MIN_CIRCLE_RADIUS or HEIGHT - py <= MIN_CIRCLE_RADIUS:
             return True
-                
+
         return False
-    
+
     def will_overlap_fig_boundary(self, canvas_pxls):
         return utils.opposite_colr_point_in_circle(self, MIN_CIRCLE_RADIUS, canvas_pxls)
-    
+
+
 class Node():
     ''' Node of the CirclesAdjacencyGraph class.
-    
+
     Attributes:
         center: Point
         radius: int := Initially set to MIN_CIRCLE_RADIUS.
         max_radius: int := Initially set to MIN_CIRCLE_RADIUS.
-        adj_nodes: List[int] := list of index (in CirclesAdjacencyGraph) of nodes adjacent to this node.
+        adj_nodes: list[int] := list of index (in CirclesAdjacencyGraph) of nodes adjacent to this node.
     '''
+
     def __init__(self, center):
         self.center = center
         self.radius = MIN_CIRCLE_RADIUS
         self.max_radius = MIN_CIRCLE_RADIUS
         self.adj_nodes = []
-        
+
     def build_adj_nodes(self, indx, node_list, canvas_pxls):
         ''' 
         Get all adjacent nodes of this node and adjust max_radius 
         accordingly.
-        
+
         Parameters:
             indx: int := Index of this node in the node_list.
-            node_list: List[Node]
-            canvas_pxls: List[color]
-            
+            node_list: list[Node]
+            canvas_pxls: list[color]
+
         Return Value:
             None
         '''
@@ -87,59 +91,62 @@ class Node():
                     adj_nodes = [i]
                 elif distance == max_radius:
                     adj_nodes.append(i)
-                    
+
         self.max_radius = max_radius
-        
+
         new_max_radius = self._nearest_fig_boundary_distance(canvas_pxls)
         if new_max_radius < self.max_radius:
             self.max_radius = new_max_radius
             adj_nodes = []
-            
+
         for index in adj_nodes:
             if index not in self.adj_nodes:
                 self.adj_nodes.append(index)
                 if indx not in node_list[index].adj_nodes:
                     node_list[index].adj_nodes.append(indx)
-        
+
     def _nearest_wall_distance(self):
         cx, cy = self.center.get_coord()
-        
+
         nearest_wall_x = cx if cx < WIDTH - cx else WIDTH - cx
         nearest_wall_y = cy if cy < HEIGHT - cy else HEIGHT - cy
         return min([nearest_wall_x, nearest_wall_y])
-            
+
     def _other_node_distance(self, n2):
         c1 = self.center.get_coord()
         c2 = n2.center.get_coord()
         return utils.distance(c1, c2) - MIN_CIRCLE_RADIUS
-    
+
     def _nearest_fig_boundary_distance(self, canvas_pxls):
         nearest_dist = self.max_radius
         c = self.center
         curr_max_radius = self.max_radius
-        opp_colr_points = utils.get_opposite_colr_points_in_circle(c, curr_max_radius, canvas_pxls)
-        
+        opp_colr_points = utils.get_opposite_colr_points_in_circle(
+            c, curr_max_radius, canvas_pxls)
+
         for p2_loc in opp_colr_points:
             p2x, p2y = utils.loc_to_coord(p2_loc)
-            
+
             distance = utils.distance(c.get_coord(), (p2x, p2y))
-            nearest_dist = min(nearest_dist, distance) 
-            
+            nearest_dist = min(nearest_dist, distance)
+
         return nearest_dist
-    
+
+
 class CirclesAdjacencyGraph:
     ''' 
     A graph with nodes representing the circles of the Ishihara Plate. Two nodes are adjacent with
     each other when at least one of them has their max_radius bounded by the other node.
-    
+
     Attributes:
-        nodes: List[Node]
+        nodes: list[Node]
     '''
+
     def __init__(self, center_points, canvas_pxls):
         self.nodes = self._get_nodes(center_points)
         for i in range(len(self.nodes)):
             self.nodes[i].build_adj_nodes(i, self.nodes, canvas_pxls)
-            
+
     def visualize(self, color_scheme):
         noStroke()
         r = MIN_CIRCLE_RADIUS
@@ -147,9 +154,10 @@ class CirclesAdjacencyGraph:
             fill(rand.choice(color_scheme))
             cx, cy = node.center.get_coord()
             ellipse(cx, cy, 2*r, 2*r)
-            
+
         for node in self.nodes:
-            colr = color(rand.uniform(0, 255), rand.uniform(0, 255), rand.uniform(0, 255))
+            colr = color(rand.uniform(0, 255), rand.uniform(
+                0, 255), rand.uniform(0, 255))
             stroke(colr)
             fill(colr)
             cx, cy = node.center.get_coord()
@@ -158,15 +166,10 @@ class CirclesAdjacencyGraph:
                 cx2, cy2 = node2.center.get_coord()
                 line(cx, cy, cx2, cy2)
                 ellipse(cx, cy, r, r)
-        
+
     def _get_nodes(self, center_points):
         nodes = []
         for p in center_points:
             nodes.append(Node(p))
-            
+
         return nodes
-    
-                
-                
-        
-    
--- gbipg.py
+++ gbipg.py
@@ -7,12 +7,14 @@ from classes import Point, CirclesAdjacencyGraph
 from const import *
 import utils
 
-def setup():        
+
+def setup():
     size(800, 800)
     background(WHITE)
-    
+
+    print('Program start.')
     start_time = time.time()
-    
+
     config = open('config.json')
     config_json = json.load(config)
     config.close()
@@ -25,67 +27,76 @@ def setup():
     if img:
         if DISPLAY_IMG:
             image(img, 0, 0)
-        GBIPG(True)
-        print('Success')    
-    else: 
-        print('Failed')
+        img.loadPixels()
+        GBIPG(img.pixels)
+        print('Success.')
+    else:
+        print('Failed.')
         exit()
-    
+
     print('Program finished execution after {} seconds.'
           .format(round(time.time() - start_time, 3))
-    )
-    
-def GBIPG(overlap_output = False):
-    ''' 
-    Generate compactly-filled, randomized circles on the background or on the
-    figure using the Graph-based Ishihara Plate Generation (GBIPG) Algorithm.
-    
-    Parameters:
-        overlap_output: boolean := If True, overlap the output of this function to the current canvas. Otherwise,
-                        clear the background and display the output. This is initially set to False.
-    '''
-    loadPixels()
-    fig_random_points, bg_random_points = generate_random_points(pixels)
+          )
 
-    fig_cag = build_circles_adjacency_graph(fig_random_points, pixels)
-    bg_cag = build_circles_adjacency_graph(bg_random_points, pixels)
-    
-    if not overlap_output: background(WHITE)
-    
-    solved_fig_cag = solve_csp_of_cag(fig_cag, visualize = True, color_scheme = FIG_COLOR_SCHEME)
-    solved_bg_cag = solve_csp_of_cag(bg_cag, visualize = True, color_scheme = BG_COLOR_SCHEME)
-        
-def generate_random_points(canvas_pxls, visualize = False, color_scheme = RED_COLOR_SCHEME):
+
+def GBIPG(img_pxls, overlap_output=False):
+    ''' 
+    Generate compactly-filled, randomized circles on the background and the 
+    figure using the Graph-based Ishihara Plate Generation (GBIPG) Algorithm.
+
+    Parameters:
+        img_pxls: list[color] := The pixels of the image reference.
+
+        overlap_output: boolean := If True, overlap the output of this function 
+                                   to the current canvas. Otherwise, clear the 
+                                   background and display the output. This is 
+                                   initially set to False.
+    '''
+    fig_random_points, bg_random_points = generate_random_points(img_pxls)
+
+    fig_cag = build_circles_adjacency_graph(fig_random_points, img_pxls)
+    bg_cag = build_circles_adjacency_graph(bg_random_points, img_pxls)
+
+    if not overlap_output:
+        background(WHITE)
+
+    solved_fig_cag = solve_csp_of_cag(
+        fig_cag, visualize=True, color_scheme=FIG_COLOR_SCHEME)
+    solved_bg_cag = solve_csp_of_cag(
+        bg_cag, visualize=True, color_scheme=BG_COLOR_SCHEME)
+
+
+def generate_random_points(canvas_pxls, visualize=False, color_scheme=RED_COLOR_SCHEME):
     '''
     Return a list of random points in the background and a list of random points in the figure. 
     The random points are generated such that they do not overlap with other points, 
     figure boundary, and the canvas wall.
-    
+
     Parameters:
-        canvas_pxls: List[color]
+        canvas_pxls: list[color]
         visualize: boolean := If True, the random points will be drawn on the canvas
                               using circles with radius MIN_CIRCLE_RADIUS. Note that 
                               this will draw over the canvas, so if the canvas' pixel 
                               information is needed, don't use this (or transfer the 
                               circles in another image). This is initially set to False.
-        color_scheme: List[str] := List of color hex strings that will be used as argument to fill().
-        
+        color_scheme: list[str] := list of color hex strings that will be used as argument to fill().
+
     Return Value:
-        (fig_random_points, bg_random_points): Tuple[List[Point], List[Point]]
+        (fig_random_points, bg_random_points): tuple[list[Point], list[Point]]
     '''
     bg_random_points = []
     fig_random_points = []
-    
+
     stroke(BLACK)
-    
+
     for i in range(MAX_NUM_CIRCLES):
         x, y = int(rand.uniform(0, WIDTH)), int(rand.uniform(0, HEIGHT))
         p = Point(x, y, canvas_pxls)
         overlap = False
-                
+
         if p.will_overlap_wall() or p.will_overlap_fig_boundary(canvas_pxls):
             overlap = True
-                                                                            
+
         if not overlap:
             # TODO: Don't check all points in random_points, only points near p.
             random_points = fig_random_points if p.in_fig() else bg_random_points
@@ -93,49 +104,51 @@ def generate_random_points(canvas_pxls, visualize = False, color_scheme = RED_CO
                 if p.will_overlap_point(p2):
                     overlap = True
                     break
-                                                    
+
         if not overlap:
             if p.in_fig():
                 fig_random_points.append(p)
             else:
                 bg_random_points.append(p)
-    
+
     if visualize:
         r = MIN_CIRCLE_RADIUS
         for p in fig_random_points + bg_random_points:
             fill(rand.choice(color_scheme))
             x, y = p.get_coord()
             ellipse(x, y, 2*r, 2*r)
-            
+
     return (fig_random_points, bg_random_points)
 
-def build_circles_adjacency_graph(center_points, canvas_pxls, visualize = False, color_scheme = [RED_COLOR_SCHEME]):
+
+def build_circles_adjacency_graph(center_points, canvas_pxls, visualize=False, color_scheme=[RED_COLOR_SCHEME]):
     ''' Build the CirclesAdjacencyGraph from the given center_points.
-    
+
     Parameters:
-        center_points: List[Point]
-        canvas_pxls: List[color]
+        center_points: list[Point]
+        canvas_pxls: list[color]
         visualize: boolean
-        color_scheme: List[str] := List of color hex strings that will be used as argument to fill().
-        
+        color_scheme: list[str] := list of color hex strings that will be used as argument to fill().
+
     Return Value:
         cag: CirclesAdjacencyGraph
     '''
     cag = CirclesAdjacencyGraph(center_points, canvas_pxls)
-    
+
     if visualize:
         cag.visualize(color_scheme)
-        
+
     return cag
 
-def solve_csp_of_cag(cag, visualize = False, color_scheme = [RED_COLOR_SCHEME]):
+
+def solve_csp_of_cag(cag, visualize=False, color_scheme=[RED_COLOR_SCHEME]):
     ''' Solve the Constraint Satisfaction Problem of the Circles Adjacency Graph cag.
-    
+
     Params:
         cag: CirclesAdjacencyGraph
         visualize: boolean
-        color_scheme: List[str] := List of color hex strings that will be used as argument to fill().
-        
+        color_scheme: list[str] := list of color hex strings that will be used as argument to fill().
+
     Return Value:
         solved_cag: CirclesAdjacencyGraph := This is cag but with the radius of each of its node
                                              satisfying the CSP of cag.
@@ -145,13 +158,14 @@ def solve_csp_of_cag(cag, visualize = False, color_scheme = [RED_COLOR_SCHEME]):
         cx, cy = cag.nodes[i].center.get_coord()
         for indx in cag.nodes[i].adj_nodes:
             cx2, cy2 = cag.nodes[indx].center.get_coord()
-            other_node_new_max_radius = utils.distance((cx, cy), (cx2, cy2)) - cag.nodes[i].radius
+            other_node_new_max_radius = utils.distance(
+                (cx, cy), (cx2, cy2)) - cag.nodes[i].radius
             if other_node_new_max_radius < cag.nodes[indx].max_radius:
                 cag.nodes[indx].max_radius = other_node_new_max_radius
                 cag.nodes[indx].adj_nodes.remove(i)
-                
+
     solved_cag = cag
-    
+
     if visualize:
         noStroke()
         for node in solved_cag.nodes:
@@ -159,10 +173,5 @@ def solve_csp_of_cag(cag, visualize = False, color_scheme = [RED_COLOR_SCHEME]):
             x, y = node.center.get_coord()
             r = node.radius
             ellipse(x, y, 2*r, 2*r)
-    
+
     return solved_cag
-            
-            
-        
-        
-        
