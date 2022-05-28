4a34c41401a9b46410a4afc650c2b02401b42fc3
Marshblocker <glmarinas@up.edu.ph>
2022-05-28 19:59:02 +0800

    Add GREYSCALE_COLOR_SCHEME, MAX_CIRCLE_RADIUS in const.py

--- const.py
+++ const.py
@@ -19,7 +19,9 @@ GREYSCALE_THRESHOLD = 127
 FIG_COLOR_SCHEME = config_json['plate']['circles']['color_scheme']['figure']
 BG_COLOR_SCHEME = config_json['plate']['circles']['color_scheme']['background']
 RED_COLOR_SCHEME = ['#ff0000']
+GREYSCALE_COLOR_SCHEME = ['#b4b4b4', '#646464', '#d4d4d4', '#4c4c4c']
 
 MIN_CIRCLE_RADIUS = config_json['plate']['circles']['min_radius']
+MAX_CIRCLE_RADIUS = config_json['plate']['circles']['mc_max_radius']
 WALL_RADIUS = config_json['plate']['wall_radius']
 BOX_SIZE = config_json['plate']['circles']['box_size']
