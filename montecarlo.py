74007a57a89c5c916bb5c32829805b07c8390adc
Marshblocker <glmarinas@up.edu.ph>
2022-05-28 16:32:00 +0800

    Refactor montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -86,22 +86,20 @@ def run(img):
 def monte_carlo(img_pxls):
     remaining_canvas_area = math.pi * WALL_RADIUS**2
     MIN_UNFILLED_AREA = remaining_canvas_area * 0.4
-    MIN_RADIUS = 3
-    MAX_RADIUS = 10
 
     noStroke()
     while remaining_canvas_area > MIN_UNFILLED_AREA:
         loadPixels()
         x, y = random.randint(0, WIDTH-1), random.randint(0, HEIGHT-1)
-        r = random.randint(MIN_RADIUS, MAX_RADIUS)
+        r = random.randint(MIN_CIRCLE_RADIUS, MAX_CIRCLE_RADIUS)
         p = Point(x, y, img_pxls)
         
         overlap = False
 
-        if will_overlap_wall((x, y)):
+        if p.will_overlap_wall():
             overlap = True
         
-        if will_overlap_something(p, r, pixels):
+        if p.will_overlap_something(r, pixels):
             overlap = True
 
         if not overlap:
@@ -111,35 +109,5 @@ def monte_carlo(img_pxls):
 
             circle_area = math.pi * r**2
             remaining_canvas_area -= circle_area
-        
-
-def will_overlap_wall(coord):
-    px, py = coord
-
-    if utils.distance_squared((px, py), (WIDTH/2, HEIGHT/2)) > WALL_RADIUS**2:
-        return True
-
-    return False
-
-def will_overlap_something(p, r, canvas_pxls):
-    return opposite_colr_point_in_circle(p, r, canvas_pxls)
-
-def opposite_colr_point_in_circle(p, r, canvas_pxls):
-    px, py = p.get_coord()
-    p_colr = BLACK_RGB if p.in_fig() else WHITE_RGB
-    r_squared = r*r
-
-    y_start = ceil(max(0, py - r))
-    x_start = ceil(max(0, px - r))
-    y_end = ceil(min(HEIGHT, py + r + 1))
-    x_end = ceil(min(WIDTH, px + r + 1))
-
-    for p2y in range(y_start, y_end):
-        for p2x in range(x_start, x_end):
-            p2_loc = WIDTH*p2y + p2x
-            if utils.distance_squared((px, py), (p2x, p2y)) <= r_squared and canvas_pxls[p2_loc] not in [BLACK_RGB, WHITE_RGB]:
-                return True
-
-    return False
 
 
